fdb7eb382143cfde25440f38b8e920378af95d45
molivas2022 <molivas2022@inf.udec.cl>
2023-10-13 18:18:45 -0300

    add: Implementación de clase Moneda y subclases

---
+++ src/main/java/org/main/Moneda/Moneda1000.java
@@ -0,0 +1,7 @@
+package org.main.Moneda;
+
+public class Moneda1000 extends Moneda {
+    public int getValor() {
+        return 1000;
+    }
+}
---
+++ src/main/java/org/main/Moneda/Moneda500.java
@@ -0,0 +1,7 @@
+package org.main.Moneda;
+
+public class Moneda500 extends Moneda {
+    public int getValor() {
+        return 500;
+    }
+}
