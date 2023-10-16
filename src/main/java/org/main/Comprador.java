5dcc2e94e0df79251bcc1de4af8a88bd1aed48be
Antonio Skorin <antonio.skorin@gmail.com>
2023-10-16 19:44:02 -0300

    Fix: Las excepciones PagoInsuficiente y NoHayProducto se manejarán en comprador.

--- src/main/java/org/main/Comprador.java
+++ src/main/java/org/main/Comprador.java
@@ -9,24 +9,29 @@ public class Comprador {
     private String sabor;
     private int vuelto;
     public Comprador(Moneda moneda, int id, Expendedor expendedor)
-            throws PagoInsuficienteException,
-            PagoIncorrectoException,
-            NoHayProductoException
+            throws PagoIncorrectoException
     {
-        sabor = "nada";
-        /* En esta llamada a comprarProducto podría terminar lanzandose una excepción. */
-        Producto compra = expendedor.comprarProducto(moneda, id);
-        sabor = compra.consumir();
-
-        vuelto = 0;
-        Moneda moneda_vuelto = expendedor.getVuelto();
-        while (moneda_vuelto != null) {
-            vuelto += moneda_vuelto.getValor();
-            moneda_vuelto = expendedor.getVuelto();
+        try {
+            Producto compra = expendedor.comprarProducto(moneda, id);
+            sabor = compra.consumir();
+        } catch (PagoInsuficienteException  | NoHayProductoException e) {
+            sabor = "nada";
+            System.err.println(e.getMessage());
+        } finally {
+            vuelto = 0;
+            /* El comprador extrae el vuelto del expendedor */
+            extraerVuelto(expendedor);
         }
     }
     public String queConsumiste() {
         return sabor;
+    }
+    private void extraerVuelto(Expendedor exp) {
+        Moneda moneda_vuelto = exp.getVuelto();
+        while (moneda_vuelto != null) {
+            vuelto += moneda_vuelto.getValor();
+            moneda_vuelto = exp.getVuelto();
+        }
     }
 
     public int getVuelto() {
--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -8,18 +8,21 @@ public class Main {
     public static void main(String[] args) {
 
         // Creamos una máquina expendedora con  4 unidades de cada producto
-        Expendedor exp = new Expendedor(4);
+        Expendedor exp = new Expendedor(0);
 
-        // Creamos una moneda de 1000 pesos
+        // Creamos una moneda de 1000 pesos.
         Moneda mon = new Moneda1000();
         System.out.println(mon); // Imprimimos la información de la moneda
+
+        // Este será nuestro comprador.
+        Comprador com;
 
         try {
             // Creamos un comprador, el cual va a intentar comprar un Snickers con la
             // moneda de 1000 pesos que le entregaremos
-            Comprador com = new Comprador(mon, Catalogo.SNICKERS.getId(), exp);
+            com = new Comprador(mon, Catalogo.SNICKERS.getId(), exp);
 
-            // Notemos que debería tener un vuelto de 200 pesos. Podemos consultarselo
+            // Le consultamos el vuelto al comprador.
             System.out.println( com.getVuelto() );
 
             // Podemos consultar tambien por el sabor del producto que compró
@@ -27,17 +30,6 @@ public class Main {
 
         } catch (PagoIncorrectoException e) {
             System.err.println(e.getMessage());
-
-        } catch (PagoInsuficienteException e) {
-            System.err.println(e.getMessage());
-            // Si la moneda fuera insuficiente, la moneda sería devuelta por el expendedor como vuelto
-            System.out.println( exp.getVuelto() );
-
-        } catch (NoHayProductoException e) {
-            System.err.println(e.getMessage());
-            // Si ya no quedaran unidades del producto, la moneda sería devuelta por el expendedor como vuelto
-            System.out.println( exp.getVuelto() );
         }
-
     }
 }
