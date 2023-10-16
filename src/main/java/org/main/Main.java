f61b7d2658c7abe3bb0dc525378b5d80562b4704
molivas2022 <molivas2022@inf.udec.cl>
2023-10-15 23:27:12 -0300

    fix: ligero cambio de la impresión de las excepciones

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -8,30 +8,35 @@ public class Main {
     public static void main(String[] args) {
 
         // Creamos una máquina expendedora con  4 unidades de cada producto
-        Expendedor exp = new Expendedor(0);
+        Expendedor exp = new Expendedor(4);
 
         // Creamos una moneda de 1000 pesos
         Moneda mon = new Moneda1000();
         System.out.println(mon); // Imprimimos la información de la moneda
 
-        Comprador com;
         try {
             // Creamos un comprador, el cual va a intentar comprar un Snickers con la
             // moneda de 1000 pesos que le entregaremos
-            com = new Comprador(mon, Catalogo.SNICKERS.getId(), exp);
+            Comprador com = new Comprador(mon, Catalogo.SNICKERS.getId(), exp);
+
             // Notemos que debería tener un vuelto de 200 pesos. Podemos consultarselo
             System.out.println( com.getVuelto() );
+
             // Podemos consultar tambien por el sabor del producto que compró
             System.out.println( com.queConsumiste() );
-        } catch (PagoIncorrectoException pie) {
-            System.err.println(pie.getMessage());
-        } catch (PagoInsuficienteException pie) {
-            System.err.println(pie.getMessage());
+
+        } catch (PagoIncorrectoException e) {
+            System.err.println(e.getMessage());
+
+        } catch (PagoInsuficienteException e) {
+            System.err.println(e.getMessage());
             // Si la moneda fuera insuficiente, la moneda sería devuelta por el expendedor como vuelto
-            System.err.println( exp.getVuelto().toString() );
+            System.out.println( exp.getVuelto() );
+
         } catch (NoHayProductoException e) {
             System.err.println(e.getMessage());
-            System.out.println( exp.getVuelto().toString() );
+            // Si ya no quedaran unidades del producto, la moneda sería devuelta por el expendedor como vuelto
+            System.out.println( exp.getVuelto() );
         }
 
     }
