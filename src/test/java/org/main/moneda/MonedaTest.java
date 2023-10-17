f7998d97a416a392bc58f1dd34c230bbdddca613
molivas2022 <molivas2022@inf.udec.cl>
2023-10-16 22:18:38 -0300

    add: Tests de clase Moneda y subclases

--- src/test/java/org/main/moneda/MonedaTest.java
+++ src/test/java/org/main/moneda/MonedaTest.java
@@ -1,14 +1,33 @@
 package org.main.moneda;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MonedaTest {
+    private Moneda m100_1, m100_2, m500_1;
+    @BeforeEach
+    void testSetup() {
+        m100_1 = new Moneda100();
+        m100_2 = new Moneda100();
+        m500_1 = new Moneda500();
+    }
     @Test
-    void  dosMonedasDe100SonIguales() {
-        Moneda100 m1 = new Moneda100();
-        Moneda100 m2 = new Moneda100();
-        assertTrue( m1.compareTo(m2) == 1 );
+    void testMonedasDelMismoValor() {
+        assertEquals(1, m100_1.compareTo(m100_2));
+    }
+    @Test
+    void testMonedasDeDistintoValor() {
+        assertNotEquals(1, m100_1.compareTo(m500_1));
+    }
+    @Test
+    void testRecibirValorDeMonedaDe500() {
+        assertEquals(500, m500_1.getValor());
+    }
+    @Test
+    void testDosMonedasDe100TienenDistintaSerie() {
+        assertNotEquals(m100_1.getSerie(), m100_2.getSerie());
     }
 }
