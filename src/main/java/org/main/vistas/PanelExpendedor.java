e81f2ab54059f2378e2940e0b79c8eebfab3b264
molivas2022 <molivas2022@inf.udec.cl>
2023-10-24 11:23:00 -0300

    PanelDeposito implemente funcionalidades para PanelProducto

--- src/main/java/org/main/vistas/PanelExpendedor.java
+++ src/main/java/org/main/vistas/PanelExpendedor.java
@@ -2,6 +2,8 @@ package org.main.vistas;
 import org.main.modelos.expendedor.Deposito;
 import org.main.modelos.expendedor.Expendedor;
 import org.main.modelos.moneda.Moneda;
+import org.main.modelos.moneda.Moneda100;
+import org.main.modelos.moneda.Moneda500;
 import org.main.modelos.productos.CocaCola;
 import org.main.modelos.productos.Producto;
 import org.main.modelos.productos.Sprite;
@@ -13,14 +15,23 @@ public class PanelExpendedor extends JPanel {
     private Expendedor exp;
     PanelExpendedor() {
         super();
-        setLayout(new FlowLayout(FlowLayout.LEFT));
+        setLayout(new GridLayout(3,1));
         setPreferredSize(new Dimension(1200, 1200));
         exp = new Expendedor(5);
         // Crear paneles deposito
         Deposito<CocaCola> cocaDep = exp.getDepCocaCola();
         PanelDeposito<CocaCola> panelCoca = new PanelDeposito<CocaCola>(CocaCola.class, cocaDep);
 
-        add(panelCoca);
+        Deposito<Sprite> spriteDep = exp.getDepSprite();
+        PanelDeposito<Sprite> panelSprite = new PanelDeposito<Sprite>(Sprite.class, spriteDep);
+
+        Deposito<Moneda> monedaDep = new Deposito<Moneda>();
+        monedaDep.addObjeto(new Moneda100()); monedaDep.addObjeto(new Moneda500());
+        PanelDeposito<Moneda> panelMoneda = new PanelDeposito<Moneda>(Moneda.class, monedaDep);
+
+        add(panelCoca, 0);
+        add(panelSprite, 1);
+        add(panelMoneda, 2);
 
     }
 }
