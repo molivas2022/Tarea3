9a1c2a20387a5df4e399e74bad62870c0035be75
Antonio Skorin <antonio.skorin@gmail.com>
2023-10-24 11:28:41 -0300

    Fix: Codigo comentado por alguna razón, arreglado.

--- src/main/java/org/main/vistas/PanelExpendedor.java
+++ src/main/java/org/main/vistas/PanelExpendedor.java
@@ -1,10 +1,10 @@
 package org.main.vistas;
+import org.main.modelos.expendedor.Deposito;
 import org.main.modelos.expendedor.Expendedor;
 import org.main.modelos.moneda.Moneda;
 import org.main.modelos.moneda.Moneda100;
 import org.main.modelos.moneda.Moneda500;
 import org.main.modelos.productos.CocaCola;
-import org.main.modelos.productos.Producto;
 import org.main.modelos.productos.Sprite;
 
 import javax.swing.JPanel;
@@ -20,9 +20,9 @@ public class PanelExpendedor extends JPanel {
         exp = new Expendedor(5);
 
         // Crear paneles deposito
-        // Deposito<CocaCola> cocaDep = exp.getDepCocaCola();
-        // PanelDeposito<CocaCola> panelCoca = new PanelDeposito<CocaCola>(CocaCola.class, cocaDep);
 
+        Deposito<CocaCola> cocaDep = exp.getDepCocaCola();
+        PanelDeposito<CocaCola> panelCoca = new PanelDeposito<CocaCola>(CocaCola.class, cocaDep);
         Deposito<Sprite> spriteDep = exp.getDepSprite();
         PanelDeposito<Sprite> panelSprite = new PanelDeposito<Sprite>(Sprite.class, spriteDep);
 
