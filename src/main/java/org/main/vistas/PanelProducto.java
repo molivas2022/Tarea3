925d1444e6d7b79fe3bd927110af1b4a57bb42af
Antonio Skorin <antonio.skorin@gmail.com>
2023-10-24 23:37:54 -0300

    Fix: Arreglado carga erronea de imagenes de productos.

--- src/main/java/org/main/vistas/PanelProducto.java
+++ src/main/java/org/main/vistas/PanelProducto.java
@@ -5,51 +5,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
 
+// TODO: Cambiarle el nombre, es un JLabel...
 public class PanelProducto extends JLabel {
     Producto producto;
     PanelProducto(Producto producto, int width, int height) {
         super();
-        ArrayList<BufferedImage> spritesProductos = loadSprites();
         setSize(width, height);
         setPreferredSize(new Dimension(getWidth(), getHeight()));
         this.producto = producto;
         BufferedImage imagen = null;
-        if (producto.consumir().equals("CocaCola")) {
-            imagen = spritesProductos.get(0);
-        } else if (producto.consumir().equals("Sprite")) {
-            imagen = spritesProductos.get(1);
-        }
+            try {
+                switch (producto.consumir()) {
+                    case "CocaCola":
+                        imagen = ImageIO.read(getClass().getResource("/CocaCola.png"));
+                        break;
+                    case "Sprite":
+                        imagen = ImageIO.read(getClass().getResource("/Sprite.png"));
+                        break;
+                    case "Fanta":
+                        imagen = ImageIO.read(getClass().getResource("/Fanta.png"));
+                        break;
+                    case "Snickers":
+                        imagen = ImageIO.read(getClass().getResource("/Snickers.png"));
+                        break;
+                    case "Super8":
+                        imagen = ImageIO.read(getClass().getResource("/Super8.png"));
+                        break;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
         Image reImg = imagen.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
         setIcon(new ImageIcon(reImg));
-    }
-
-    private ArrayList<BufferedImage> loadSprites() {
-        System.out.println("Cargando imagenes");
-        ArrayList<BufferedImage> spriteRet = new ArrayList<>();
-        try {
-            switch (producto.consumir()) {
-                case "CocaCola":
-                    imagen = ImageIO.read(getClass().getResource("/CocaCola.png")); break;
-                case "Sprite":
-                    imagen = ImageIO.read(getClass().getResource("/Sprite.png")); break;
-                case "Fanta":
-                    imagen = ImageIO.read(getClass().getResource("/Fanta.png")); break;
-                case "Snickers":
-                    imagen = ImageIO.read(getClass().getResource("/Snickers.png")); break;
-                case "Super8":
-                    imagen = ImageIO.read(getClass().getResource("/Super8.png")); break;
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-        try {
-            BufferedImage spriteSprite = ImageIO.read(getClass().getResource("/Sprite.png"));
-            spriteRet.add(spriteSprite);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-        return spriteRet;
     }
 }
